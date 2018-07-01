bd3d37732914d2f7fbca2a00c380362905e79553
Dieter Geerts <dieter@dworks.be>
2018-07-01 18:15:17 +0200

    Add loading of docs/previews as html files

--- packages/builder-example/src/index.js
+++ packages/builder-example/src/index.js
@@ -1,1 +1,9 @@
 console.log('[@fabricator/builder-example] Example loaded...');
+
+const requireDocs = require.context('.', true, /\.docs\.html$/);
+
+console.log('[@fabricator/builder-example] Docs to require: ', requireDocs.keys());
+
+const requiredDocs = requireDocs.keys().map(requireDocs);
+
+console.log('[@fabricator/builder-example] Docs required: ', requiredDocs);
--- packages/builder-example/webpack.config.js
+++ packages/builder-example/webpack.config.js
@@ -1,5 +1,30 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// When using html to write your docs/previews, these loaders can be used,
+// to gather the html, extract, save as file and return the file location.
+const docsAndPreviewsAsHtml = {
+  test: /\.(docs|preview)\.html$/,
+  use: [
+    { loader: 'file-loader' },
+    { loader: 'extract-loader' },
+    {
+      loader: 'html-loader',
+      options: {
+        attrs: [
+          'iframe:src',
+        ],
+      },
+    },
+  ],
+};
+
 module.exports = {
-    plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      docsAndPreviewsAsHtml,
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ],
 };
