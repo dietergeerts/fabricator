20f141f1f1b563334a2859a8f366d2ea8264c7a8
Dieter Geerts <dieter@dworks.be>
2018-07-04 19:21:38 +0200

    Create navs and iframe in theme, to show stories

--- packages/builder-core/index.js
+++ packages/builder-core/index.js
@@ -1,13 +1,35 @@
+const _map = require('lodash/fp/map');
+
 /**
  * @typedef {Object} FabricatorBuilderOptions
- * @property {function(): string} Node
+ * @property {function(Array<FabricatorBuilderStoryNav>): string} theme
+ * @property {Array<FabricatorBuilderStory>} stories
  */
+
+/**
+ * @typedef {Object} FabricatorBuilderStory
+ * @property {string} filename - original source file
+ * @property {string} story - resulting filename
+ */
+
+/**
+ * @typedef {Object} FabricatorBuilderStoryNav
+ * @property {string} label
+ * @property {string} href
+ */
+
+/**
+ * @param {Array<FabricatorBuilderStory>} stories
+ * @returns {Array<FabricatorBuilderStoryNav>}
+ */
+const toStoryNavs = _map(story => ({ label: story.filename, href: story.story }));
+
 
 /**
  * @param {FabricatorBuilderOptions} options
  */
 module.exports = function fabricatorBuild(options) {
   console.log('[@fabricator/builder-core] Building toolkit...');
-  document.body.appendChild(options.theme());
+  document.body.appendChild(options.theme(toStoryNavs(options.stories)));
   console.log('[@fabricator/builder-core] Toolkit built');
 };
--- packages/builder-theme-default/index.js
+++ packages/builder-theme-default/index.js
@@ -1,7 +1,74 @@
-module.exports = function fabricatorTheme() {
+const _forEach = require('lodash/fp/forEach');
+
+/**
+ * @returns {HTMLElement}
+ */
+const createRoot = () => {
+  const root = document.createElement('div');
+  root.style.position = 'absolute';
+  root.style.width = '100%';
+  root.style.height = '100%';
+  root.style.display = 'grid';
+  root.style.gridTemplateColumns = 'auto 1fr';
+  root.style.gridGap = '1rem';
+  return root;
+};
+
+/**
+ * @returns {HTMLIFrameElement}
+ */
+const createIFrame = () => {
+  const iframe = document.createElement('iframe');
+  iframe.width = '100%';
+  iframe.height = '100%';
+  return iframe;
+};
+
+/**
+ * @param {FabricatorBuilderStoryNav} storyNav
+ * @param {function(href:string): void} onclick
+ * @returns {HTMLElement}
+ */
+const createNavLink = (storyNav, onclick) => {
+  const link = document.createElement('a');
+  link.appendChild(document.createTextNode(storyNav.label));
+  link.style.display = 'block';
+  link.href = '';
+  link.onclick = ($event) => {
+    $event.preventDefault();
+    $event.stopPropagation();
+    onclick(storyNav.href);
+  };
+  return link;
+};
+
+/**
+ * @param {Array<FabricatorBuilderStoryNav>} storyNavs
+ * @param {function(href:string): void} onclick
+ * @returns {HTMLElement}
+ */
+const createNav = (storyNavs, onclick) => {
+  const nav = document.createElement('nav');
+  _forEach(storyNav => nav.appendChild(createNavLink(storyNav, onclick)), storyNavs);
+  return nav;
+};
+
+/**
+ * @param {Array<FabricatorBuilderStoryNav>} storyNavs
+ * @returns {HTMLElement}
+ */
+module.exports = function fabricatorTheme(storyNavs) {
   console.log('[@fabricator/builder-theme-default] Building theme...');
   console.log('[@fabricator/builder-theme-default] Theme built');
-  const theme = document.createElement('h1');
-  theme.innerHTML = 'FABRICATOR THEME DEFAULT';
-  return theme;
+  const root = createRoot();
+  const iframe = createIFrame();
+  const nav = createNav(
+    storyNavs,
+    href => {
+      iframe.src = href
+    },
+  );
+  root.appendChild(nav);
+  root.appendChild(iframe);
+  return root;
 };
