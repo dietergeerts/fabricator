e9e982f523b1e4cf07b28c0714220295a4dc0277
Dieter Geerts <dieter@dworks.be>
2018-07-08 10:00:59 +0200

    Use github styling for the documentation pages

--- webpack.config.js
+++ webpack.config.js
@@ -89,7 +89,7 @@ module.exports = options => {
                   // Notice the `markdown-body` class, which is needed for the github css.
                   // TODO: Make sure this is gotten in another way and embedded into a real theme.
                   styleDocs: path.resolve(__dirname, 'node_modules/github-markdown-css/github-markdown.css'),
-                  styleDocsContainer: path.resolve(__dirname, './theme-docs.css'),
+                  styleDocsContainer: path.resolve(__dirname, './theme/docs.css'),
                   styleCode: path.resolve(__dirname, 'node_modules/highlight.js/styles/github-gist.css'),
                 },
               },
